99b84e232d629f788238ca8889614aeff161b81e
WardaRafique <124115849+WardaRafique@users.noreply.github.com>
2023-02-03 08:38:54 +0500

    Update Conditional statement (if-else conditon).cpp

--- Conditional statement (if-else conditon).cpp
+++ Conditional statement (if-else conditon).cpp
@@ -1,17 +1,27 @@
 #include <iostream>
+//preprocessor directive
 using namespace std;
 int main()
+	//main function
 {
+//opening braces
 	int time;
+	//declare variable
 	time=20;
+	//assign the value to variable
 	if(time<15)
+	//if time is less than 15
 	{
 		cout<<"Good Morning";
+		//print it,if it is true
 	}
 	else
+	//if time is greater than 15
 {
 	cout<<"Good Evening";
+	//print it, if it is true
 }
 	
 	return 0;
 }
+//closing braces
