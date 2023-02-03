2ec4287d874a7712542feeb5ba95a0b83e4e7792
WardaRafique <124115849+WardaRafique@users.noreply.github.com>
2023-02-03 08:34:15 +0500

    Update Conditional statement in c++(if condition).cpp

--- Conditional statement in c++(if condition).cpp
+++ Conditional statement in c++(if condition).cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+//preprosessor directive
 using namespace std;
 int main()
+//main function
 {
+//opening braces
 	int x,y;
+	//declare two variables
 	x=10;
+	//assign value to variable x
 	y=20;
+	//assign value to varaible y
 	if(x<y)
+        //if condition 
 	{
+	
 		cout<<"x is less than y";
+		//print it,if the statement is true
 	}
+	
 	return 0;
 }
+//closing braces
