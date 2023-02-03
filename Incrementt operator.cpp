8767e9fc2b26a3e71c2ad99bbbd1f6bad77600e5
WardaRafique <124115849+WardaRafique@users.noreply.github.com>
2023-02-03 07:57:38 +0500

    Add files via upload

---
+++ Incrementt operator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//preprosessor directive
+using namespace std;
+int main()
+//main function
+{
+	//opening braces
+	int x;
+	x=5;
+	//assign value to x
+	++x;
+	//pre_increment
+	x++;
+	//post_increment
+	cout<<x;
+	//pront the value of x
+	return 0;
+}
+//closing braces
