a925be901e4122b14aa9bbd67dcbffcd80213c44
WardaRafique <124115849+WardaRafique@users.noreply.github.com>
2023-02-03 08:23:03 +0500

    Add files via upload

---
+++ Decrement operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//preprosessor directive
+using namespace std;
+int main()
+//main function
+{
+	//opening braces
+		int x=5;
+	//assign value to x
+	--x;
+	//pre_decrement
+	x--;
+	//post_decrement
+	cout<<x ;
+	//pront the value of x
+	return 0;
+}
+//closing braces
